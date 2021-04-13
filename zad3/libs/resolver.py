c590229efddbc727750594f8e7c087a6088c67c6
Maksymilian Kadukowski <maksymilian.kadukowski@gmail.com>
2021-04-13 15:39:20 +0200

    added tabu search

--- zad3/libs/resolver.py
+++ zad3/libs/resolver.py
@@ -1,10 +1,18 @@
+from dataclasses import dataclass
+from random import randint
 from itertools import permutations, chain
 from collections import deque
+from typing import Tuple
+from time import time
 
-from .order import Order
+import numpy as np
+from numpy.core.fromnumeric import _swapaxes_dispatcher
+
+from .order import NpOrder, Order
 from .grouped_tasks import GroupedTasks
-from .helpers import get_c_max, pick_min, decay_grouped_tasks_to_2_machines, get_sorted_task_order, task_processing_time_on_all_machines, insert_into_task_order
-from .helpers import get_partial_c_max
+from .helpers import (get_c_max, pick_min, decay_grouped_tasks_to_2_machines, get_sorted_task_order,
+    task_processing_time_on_all_machines, get_partial_c_max)
+
 
 
 
@@ -86,3 +94,106 @@ class NehResolver(Resolver):
             current_order.order.insert(best_order_for_index, order.order[task])
 
         return current_order
+
+
+class TsResolver(Resolver):
+    def __init__(self, neighbours_max, first_order=None):
+        self.neighbours_max = neighbours_max
+        self.first_order = first_order
+
+    def gen_idx(self, max):
+        idx_a = randint(0, max)
+        idx_b = randint(0, max)
+        while idx_a == idx_b:
+            idx_b = randint(0, max)
+        return (idx_a, idx_b)
+
+    class SwapDecision:
+        def __init__(self, idx_a, idx_b):
+            self.idx_a = idx_a
+            self.idx_b = idx_b
+
+        def __call__(self, order: Order):
+            f = order.order
+            f[self.idx_a], f[self.idx_b] = f[self.idx_b], f[self.idx_a]
+
+
+    def random_swap_decision(self, order):
+        return self.SwapDecision(*self.gen_idx(len(order.order) - 1))
+
+    def gen_order_decision(self, order: Order) -> Tuple[Order, SwapDecision]:
+        for _ in range(self.neighbours_max):
+            swap_decision = self.random_swap_decision(order)
+
+            # apply swap on current order
+            swap_decision(order)
+
+            # yield order only for c_max evaulation
+            # as it will change
+            yield (order, swap_decision)
+
+            # revert to original order
+            swap_decision(order)
+
+
+    class TabuList:
+        def __init__(self, max_size):
+            self.max_size = max_size
+            self.tabu_history = deque()
+            self.tabu_set = set()
+
+        def __contains__(self, obj):
+            #return obj in self.tabu_set
+            return obj in self.tabu_set
+
+        def add(self, obj):
+            assert obj not in self
+
+            if len(self.tabu_history) >= self.max_size:
+                self.tabu_set.remove(self.tabu_history[0])
+                self.tabu_history.popleft()
+
+            self.tabu_history.append(obj)
+            self.tabu_set.add(obj)
+
+
+    def resolve(self, grouped_tasks: GroupedTasks) -> Order:
+        current = self.first_order
+        current = NpOrder(np.array(current.order))
+        best = NpOrder(np.copy(current.order))
+        best_c_max = get_c_max(grouped_tasks, best)
+
+        tabu_list = self.TabuList(1000)
+        stop_time = time() + .5 * 60
+
+        iteration = 0
+
+        while time() < stop_time:
+            iteration += 1
+
+            order_decisions = ((order, swap_decision) for order, swap_decision in self.gen_order_decision(current)
+                if order not in tabu_list)
+
+            # order is dynamically changed current
+            _, decision = min(order_decisions, default=(None, None), key=lambda order_and_decision: get_c_max(grouped_tasks, order_and_decision[0]))
+
+            if decision is None:
+                continue
+
+            # generate min order in current
+            decision(current)
+
+
+            # add to tabu list
+            not_writeable_copy = np.copy(current.order)
+            not_writeable_copy.flags.writeable = False
+            tabu_list.add(NpOrder(not_writeable_copy))
+
+            current_c_max = get_c_max(grouped_tasks, current)
+            if current_c_max < best_c_max:
+                best_c_max = current_c_max
+                best = NpOrder(np.copy(current.order))
+
+        print(f'Tabu list iterations: {iteration}')
+
+        return best
--- zad3/main.py
+++ zad3/main.py
@@ -2,31 +2,34 @@ import datetime
 
 from libs.helpers import get_c_max, create_random_grouped_task
 from libs.load_file import load_file
-from libs.resolver import NehResolver, BruteForceResolver, JohnsonResolver
+from libs.resolver import NehResolver, BruteForceResolver, JohnsonResolver, TsResolver
 
 #from libs.gantt_plot import GanttPlot
 
 def main():
     #plot = GanttPlot()
-    task = load_file("data114.txt")
+    task = load_file("data102.txt")
 
     #plot.InsertTaskGroup(task)
 
     start = datetime.datetime.now()
     print("START: " + str(start))
     #order_brute = BruteForceResolver().resolve(task)
-    #order_johnson = JohnsonResolver().resolve(task)
-    order_neh = NehResolver().resolve(task)
+    order_johnson = JohnsonResolver().resolve(task)
+    #order_neh = NehResolver().resolve(task)
+    order_tabu = TsResolver(10, order_johnson).resolve(task)
     end = datetime.datetime.now()
     delta = end - start
 
     #cmax_brute = get_c_max(task, order_brute)
-    #cmax_johnson = get_c_max(task, order_johnson)
-    cmax_neh = get_c_max(task, order_neh)
+    cmax_johnson = get_c_max(task, order_johnson)
+    #cmax_neh = get_c_max(task, order_neh)
+    cmax_tabu = get_c_max(task, order_tabu)
 
     #print("BRUTE: " + str(cmax_brute))
-    #print("JOHNSON: " + str(cmax_johnson))
-    print("NEH: " + str(cmax_neh))
+    print("JOHNSON: " + str(cmax_johnson))
+    #print("NEH: " + str(cmax_neh))
+    print(f"TABU: {cmax_tabu}")
     print("ELAPSED: " + str(delta))
 
 
@@ -35,9 +38,6 @@ def main():
     #plot.InsertOrder(order_brute)
     #plot.Show("Brute")
     #plot.InsertOrder(order_neh)
-    for i in order_neh.order:
-        print(str(i+1), end=' ')
-    print()
 
     #plot.Show("Neh")
 
